c6da76427f486a6379514b6ccbdab268318d0536
Nicholas Kuhle <101841832+namethepricellc@users.noreply.github.com>
2023-04-11 22:13:51 +0000

    initial commit for next05

---
+++ next05/app/[searchTerm]/error.tsx
@@ -0,0 +1,30 @@
+'use client'; // Error components must be Client components
+
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <main className="bg-slate-200 mx-auto max-w-lg py-1 px-4 min-h-screen">
+      <h2 className="my-4 text-2xl font-bold">Something went wrong!</h2>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </main>
+  );
+}
---
+++ next05/app/[searchTerm]/loading.tsx
@@ -0,0 +1,9 @@
+export default function loading() {
+  return (
+    <main className="bg-slate-200 mx-auto max-w-lg py-1 px-4 min-h-screen">
+      <h2 className="my-4 text-2xl font-bold">
+        Loading, you asshole!
+      </h2>
+    </main>
+  )
+}
